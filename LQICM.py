394b89713fc040a3f2009b299dd496e8a410e899
assellak <assellak@augusta.edu>
2021-02-19 11:58:29 -0500

    added doctests for examples

--- LQICM.py
+++ LQICM.py
@@ -104,4 +104,4 @@ if __name__ == "__main__":
         optimize(sys.argv[1])
     else:
         import doctest
-        doctest.testfile("test_examples.txt", raise_on_error=True)
+        doctest.testfile("test_examples.txt")
