a964b8cc073d8e92e0a3de44c348e1a1b476bf17
assellak <assellak@augusta.edu>
2021-02-19 11:53:02 -0500

    added examples

---
+++ examples/List_infinite_source.c
@@ -0,0 +1,10 @@
+int main(){
+    int i = 0, a = 0, b = 0, c = 0;
+    int array[100];
+    while(i<100){
+        a = array[i];
+        b = c;
+        c = 90;
+        i = i +1;
+    }
+}
---
+++ output/opt_List_infinite_source.c
@@ -0,0 +1,31 @@
+int main()
+{
+  int i = 0;
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  int array[100];
+  if (i < 100)
+  {
+    a = array[i];
+    b = c;
+    c = 90;
+    i = i + 1;
+  }
+
+  if (i < 100)
+  {
+    a = array[i];
+    b = c;
+    i = i + 1;
+  }
+
+  while (i < 100)
+  {
+    a = array[i];
+    i = i + 1;
+  }
+
+}
+
+
